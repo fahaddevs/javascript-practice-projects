a831f208c615fad198352058f006c511b06e1043
fahaddevs <fahaddevs@gmail.com>
2023-09-22 21:32:06 +0600

    define all selectors

---
+++ task-list/app.js
@@ -0,0 +1,7 @@
+// Define all selectors
+let takForm = document.querySelector('#task_form');
+let taskInput = document.querySelector('#task_input');
+
+let taskList = document.querySelector('#task_list');
+let filterTask = document.querySelector('#task_filter');
+let removeTask = document.querySelector('#clear_task');
