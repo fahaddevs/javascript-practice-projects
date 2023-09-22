0bdb454ccd5edfb2fc9d6a729830d77144a3f5c1
fahaddevs <fahaddevs@gmail.com>
2023-09-22 21:33:13 +0600

    add all event listener

--- task-list/app.js
+++ task-list/app.js
@@ -5,3 +5,9 @@ let taskInput = document.querySelector('#task_input');
 let taskList = document.querySelector('#task_list');
 let filterTask = document.querySelector('#task_filter');
 let removeTask = document.querySelector('#clear_task');
+
+
+// Add event listener
+takForm.addEventListener('submit', addTaskFunc);
+taskList.addEventListener('click', removeTaskFunc);
+removeTask.addEventListener('click', removeAllTaskFunc);
